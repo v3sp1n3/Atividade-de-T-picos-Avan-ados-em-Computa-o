8a66378e9d0baf002edeb865e099fa66e27525c3
Nycolas Guilherme <118767547+v3sp1n3@users.noreply.github.com>
2026-06-12 20:49:10 -0300

    Create ConsultaRepository.java

---
+++ main/java/com/exemplo/demo/repository/ConsultaRepository.java
@@ -0,0 +1,8 @@
+package com.exemplo.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+}
